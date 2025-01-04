71284db0c27e1e9874b79dac671be824af1976db
Marina.S. <marina.sevostian@gmail.com>
2025-01-04 09:37:31 +0200

    added task 1 without styles and structure for task 2

---
+++ src/components/App.jsx
@@ -0,0 +1,26 @@
+// import { useState } from "react";
+// import reactLogo from "./assets/react.svg";
+// import viteLogo from "/vite.svg";
+import "./App.css";
+import Profile from "./Task-1/Profile";
+import userData from "../userData.json";
+import FriendList from "./Task-2/FriendList";
+import TransactionHistory from "./Task-3/TransactionHistory";
+
+function App() {
+  return (
+    <>
+      <Profile
+        name={userData.username}
+        tag={userData.tag}
+        location={userData.location}
+        image={userData.avatar}
+        stats={userData.stats}
+      />
+      <FriendList />
+      <TransactionHistory />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Task-1/Profile.jsx
@@ -0,0 +1,35 @@
+import {} from "./Profile.module.css";
+
+const Profile = ({
+  name,
+  tag,
+  location,
+  image = "https://cdn-icons-png.flaticon.com/512/1077/1077012.png",
+  stats,
+}) => {
+  return (
+    <div>
+      <div>
+        <img src={image} alt="User avatar" />
+        <p>{name}</p>
+        <p>@{tag}</p>
+        <p>{location}</p>
+      </div>
+      <ul>
+        <li>
+          <span>Followers</span>
+          <span>{stats.followers}</span>
+        </li>
+        <li>
+          <span>Views</span>
+          <span>{stats.views}</span>
+        </li>
+        <li>
+          <span>Likes</span>
+          <span>{stats.likes}</span>
+        </li>
+      </ul>
+    </div>
+  );
+};
+export default Profile;
